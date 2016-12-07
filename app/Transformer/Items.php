a08447d50226cf90e36b7aa67b25ffe473f4d128
Harry Sitohang <harry@olx.co.id>
2016-12-08 06:43:53 +0700

    implement array_filter

--- app/Transformer/Items.php
+++ app/Transformer/Items.php
@@ -5,26 +5,30 @@ class Items extends \League\Fractal\TransformerAbstract
 {
     public function transform(\App\Items $items)
     {
-        return [
+        return array_filter([
             'id' => $items->id,
             'status' => $items->status,
             'item_name' => $items->name,
             'receiver' => [
                 'name' => $items->receiver_name,
                 'phone_number' => $items->receiver_phone_number,
-                'address' => $items->destination_address,
             ],
             'sender' => [
                 'id' => $items->id_customer,
-                'address' => $items->pickup_address,
+                'link' => route('users.show', [$items->id_customer]),
             ],
-            'kurir' => [
+            'address' => [
+                'pickup' => $items->pickup_address,
+                'destination' => $items->destination_address,
+            ],
+            'kurir' => array_filter([
                 'id' => $items->id_kurir,
-            ],
-            'timestamp' => [
+                'link' => !is_null($items->id_kurir) ? route('users.show', [$items->id_kurir]) : null
+            ]),
+            'timestamp' => array_filter([
                 'created' => $items->created_at, //$user->created_at->toDateTimeString()
                 'updated' => $items->updated_at, //$user->updated_at->toDateTimeString()
-            ]
-        ];
+            ])
+        ]);
     }
 }
