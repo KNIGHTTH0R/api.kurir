398449597bf5c36811a5c7f0cfcb5e2eeea9a52c
Harry Sitohang <harry@olx.co.id>
2016-12-07 09:43:48 +0700

    remove insert on user index endpoint

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,33 +13,6 @@ class UsersController extends Controller
      */
     public function index()
     {
-        \DB::table('items')->insert(
-            [
-                'name' => 'samsung edge 7',
-                'receiver_name' => 'saiful',
-                'receiver_phone_number' => '+625254318976',
-                'pickup_address' => 'gajahmada plaza, sawah besar, jakarta pusat',
-                'destination_address' => 'pasaraya blok m, jakarta selatan',
-                'status' => 'new',
-            ],
-            [
-                'name' => 'macbook pro Retina 15 inch',
-                'receiver_name' => 'sulaiman',
-                'receiver_phone_number' => '+621309876549',
-                'pickup_address' => 'Mangga 2 square, Jakarta pusat',
-                'destination_address' => 'cinere mall, depok jawa barat',
-                'status' => 'new',
-            ],
-            [
-                'name' => 'nasi goreng bakmi gm',
-                'receiver_name' => 'kartono',
-                'receiver_phone_number' => '+621344553322',
-                'pickup_address' => 'Bakmi GM blok m, seberang menara sentraya',
-                'destination_address' => 'pom bensin radio dalam',
-                'status' => 'new',
-            ]
-        );
-
         return ['a' => 'b'];
     }
 
